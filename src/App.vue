eddc8febb512c60d65b8ec894dbde7ab284a6342
kruvv <kruvv@mail.ru>
2024-11-28 12:29:12 +0300

    Init app

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script lang="ts"></script>
+
+<template>
+  <div class="wrapper">
+    <h1>Погодное приложение</h1>
+    <p>Узнать погоду в вашем городе</p>
+  </div>
+</template>
+
+<style scoped></style>
